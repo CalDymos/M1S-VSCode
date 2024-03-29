d342f2ea88aba622c778a9cecf6604e33f07e4b8
CalDymos <65937830+CalDymos@users.noreply.github.com>
2024-03-29 10:21:43 +0100

    Update extension.js

--- dist/extension.js
+++ dist/extension.js
@@ -45,6 +45,75 @@ function activate(context) {
     vscode_1.commands.registerCommand("m1s.killScript", () => {
         cmds.killScript();
     });
+    	
+    let buttonActivation = vscode.commands.registerTextEditorCommand('extension.indent', (editor) => {
+		let document = editor.document
+		prepareDocument(document)
+	});
+
+	context.subscriptions.push(buttonActivation);
+
+	let formatFunction = vscode.languages.registerDocumentFormattingEditProvider('m1s', {
+		provideDocumentFormattingEdits: (document) => {
+			prepareDocument(document)
+		}
+	});
+
+	context.subscriptions.push(formatFunction);
 }
-exports.activate = activate;
+
+function prepareDocument(document) {
+
+	if (editor && editor.document.languageId === 'm1s') {
+
+		var inFile = document.fileName;
+		const documentText = document.getText();
+		const fileExtension = getFileExtension(inFile);
+		const start = new Position(getStartLine(documentText, fileExtension), 0);
+		const end = new Position(document.lineCount + 1, 0);
+		const range = new Range(start, end);
+		const sourceFile = document.getText(range);
+
+		vscode.window.showInformationMessage('Formatting Script');
+
+		let outFile = m1sindent({
+			level: levelValue,
+			indentChar: indentCharValue,
+			breakLineChar: breakLineCharValue,
+			breakOnSeperator: breakOnSeperatorValue,
+			removeComments: removeCommentsValue,
+			source: sourceFile,
+		});
+		console.log(outFile);
+		const edit = new vscode.WorkspaceEdit();
+		edit.replace(document.uri, range, outFile);
+		return vscode.workspace.applyEdit(edit)
+
+	} else {
+		vscode.window.showInformationMessage('Not a Mach3 Script file!');
+	}
+}
+
+function getStartLine(text, fileExtension) {
+
+	/*if (fileExtension === '.m1s') {
+		const lineNumber = findLineNumber(text, '#expand');
+		return lineNumber;
+	}*/
+	//else {
+		return 0;
+	//}
+}
+
+function findLineNumber(text, searchText) {
+	const lines = text.split('\n');
+	const lineNumber = lines.findIndex(line => line.includes(searchText)) + 1;
+	return lineNumber;
+}
+
+function getFileExtension(fileName) {
+	return path.extname(fileName);
+}
+
+module.exports = {activate}
 //# sourceMappingURL=extension.js.map
